e62fb806ebdf861d4543954c82fb428213836dcc
HanDongWook <jja9312345@gmail.com>
2026-01-03 00:31:04 +0900

    Add strategy pattern implementation and tests

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,9 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.ContextV1;
+import hello.advanced.trace.strategy.code.Strategy;
+import hello.advanced.trace.strategy.code.StrategyLogic1;
+import hello.advanced.trace.strategy.code.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +34,20 @@ public class ContextV1Test {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
     }
 }
